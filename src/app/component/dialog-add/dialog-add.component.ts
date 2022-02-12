20dc3d5d887d425ee9b4aaf67503b8bc6e9cae25
gabriel ramos <gabriel.ramos@petlove.com.br>
2022-02-12 15:36:40 -0300

    add form, pegando valor do objeto a ser adicionado

--- src/app/component/dialog-add/dialog-add.component.ts
+++ src/app/component/dialog-add/dialog-add.component.ts
@@ -1,13 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 
 @Component({
   selector: 'elp-dialog-add',
   templateUrl: './dialog-add.component.html',
   styleUrls: ['./dialog-add.component.scss'],
 })
-export class DialogAddComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+
+export class DialogAddComponent implements OnInit {
+
+  public registerForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      nome: ['', [Validators.required]],
+      qtd: ['', [Validators.required]],
+      uso: ['', [Validators.required]],
+      potencia:['', [Validators.required]],
+    });
+  }
+
+  salvarEletrodomestico(){
+    console.log(this.registerForm.value)
+  }
 }
--- src/app/component/dialog-add/dialog-add.module.ts
+++ src/app/component/dialog-add/dialog-add.module.ts
@@ -5,13 +5,22 @@ import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
 import { DialogAddComponent } from './dialog-add.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [DialogAddComponent],
   exports: [DialogAddComponent],
-  imports: [CommonModule, MatTableModule, MatIconModule, MatDialogModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatIconModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class DialogAddModule {}
